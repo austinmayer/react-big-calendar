d77b92d4fe98079ee49601aaa300ef79d3889773
Austin Mayer <austin.mayer@lvthn.com>
2017-01-19 11:43:41 -0600

    build option

--- .storybook/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@kadira/storybook';
-
-function loadStories() {
-  require('../src/.stories');
-  // require as many as stories you need.
-}
-
-configure(loadStories, module);
